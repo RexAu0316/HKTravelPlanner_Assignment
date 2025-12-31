2a47ef9e2d4598767b59744263e46508e1a97391
RexAu0316 <160446866+RexAu0316@users.noreply.github.com>
2025-12-31 16:32:00 +0800

    Add files via upload
    SetUp Github

---
+++ HKTravelPlanner_Assignment/HKTravelPlanner_Assignment/HKTravelPlanner_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKTravelPlanner_AssignmentApp.swift
+//  HKTravelPlanner_Assignment
+//
+//  Created by Rex Au on 31/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct HKTravelPlanner_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
